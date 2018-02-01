702c7a15fe61751e803054861ca2bae56de0d067
zampolli75 <jrod122@lsu.edu>
2018-02-01 11:43:48 -0800

    wrangle packages to keep only those installed in default library. save objects created in the data folder

--- R/02_wrangle-packages.R
+++ R/02_wrangle-packages.R
@@ -1,14 +1,20 @@
 ## create a data frame from data/installed-packages.csv
 ## with, e.g., readr::read_csv() or read.csv()
+library(tidyverse)
+library(here)
+
+pkg <- readr::read_csv(file = here("data", "installed-packages.csv")) %>%
+  as_tibble()
 
 ## filter out packages in the default library
 ## keep variables Package and Built
 ## if you use dplyr, code like this will work:
-apt <- ipt %>%
+apt <- pkg %>%
   filter(LibPath == .libPaths()[1]) %>%
   select(Package, Built)
 
 ## write this new, smaller data frame to data/add-on-packages.csv
+write_csv(x = apt, here("data", "add-on-packages.csv"))
 
 ## make a frequency table of package by the version in Built
 ## if you use dplyr, code like this will work:
@@ -20,6 +26,7 @@ apt_freqtable <- apt %>%
 ## YES overwrite the files that are there now
 ## they came from me (Jenny)
 ## they are just examples
+write_csv(x = apt_freqtable, here("data", "add-on-packages-freqtable.csv"))
 
 ## when this script works, stage & commit it and the csv files
 ## PUSH!
